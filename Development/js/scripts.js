5eb0c31f1b5ab33062f9a083b67db1c83593f680
Abdulazizalmousawi-359 <66692273+Abdulazizalmousawi-359@users.noreply.github.com>
2020-06-29 23:41:04 +0300

    تم حل الكلاسورك

---
+++ Development/js/scripts.js
@@ -0,0 +1,70 @@
+let food =prompt("ما هو رقم طبقك");
+
+switch(food) {
+    case "1":
+      alert("بيتزا");
+      break;
+    case "2":
+    alert("دجاج");
+      break;
+      case ("3"):
+    alert("بيتزا");
+        break;
+  }
+  
+
+  var a = parseInt(prompt('enter your first number'));
+var b = prompt('enter your operator like +,-,*,/');
+var c = parseInt(prompt('enter your second number'));
+
+  switch (b) {
+    case ('+'):
+    b=a+c;
+    document.write('your answer is : ' + b);
+    break;
+   
+    case ('-'):
+    b=a-c;
+    document.write('your answer is : '+b);
+    break;
+   
+    case ('*'):
+    b=a*c;
+    document.write('your answer is : '+b);
+    break;   
+   
+    case ('/'):
+    b=a/c;
+    document.write('your answer is : '+b);
+    break;
+   
+    default:
+    document.write('you enter incorrect symbol');
+   }
+      
+   let age = prompt("كم عمرك؟")
+   
+   if (age < 7) {
+     alert("روضة")
+     
+   }
+
+   if (age<18) {
+     alert("مدرسة")
+   }
+
+   if (age>18) {
+     alert("جامعة")
+     
+   }
+  
+
+
+ 
+  
+
+    
+
+
+
+
